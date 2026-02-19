c44aa71d6cd2b445c4265a70715bdf226d5fce1e
Nathanael Wong <natgeo.wong@outlook.com>
2026-02-18 23:34:15 -0500

    Updates to ARM compilation into diurnal

--- scripts/diurnalARMwc.jl
+++ scripts/diurnalARMwc.jl
@@ -9,13 +9,16 @@ ads = ARMDataset(
     start = Date(2011), stop = Date(2021,12,31), path = datadir()
 )
 
-data = zeros(596,21600,12)
-inum = zeros(596,21600,12)
-z    = zeros(Float32,596)
+nz = 596
+nt = 21600
+
+data = zeros(nz,nt,12)
+inum = zeros(Int32,nz,nt,12)
+z    = zeros(Float32,nz)
 
 vname = "liquid_water_content"
 
-dtvec = Date(2011) : Day(1) : Date(2021,12,31); ndt = length(dtvec)
+dtvec = ads.start : Day(1) : ads.stop; ndt = length(dtvec)
 attribs = Vector{Dict}(undef,4)
 
 for idt in dtvec
@@ -27,9 +30,9 @@ for idt in dtvec
         attribs[2] = Dict(ids["height"].attrib)
         attribs[3] = Dict(ids["time"].attrib)
         attribs[4] = Dict(ids[vname].attrib)
-        z[:]           =                  ids["height"][:]
-        data[:,:,imo] +=        nomissing(ids[vname][:,:],0)
-        inum[:,:,imo] += isnan.(nomissing(ids[vname][:,:],NaN))
+        z[:]           =                    ids["height"][:]
+        data[:,:,imo] +=          nomissing(ids[vname][:,:],0)
+        inum[:,:,imo] += .!isnan.(nomissing(ids[vname][:,:],NaN))
         close(ids)
     end
 
@@ -39,19 +42,21 @@ data ./= inum
 ydata  = dropdims(mean(data,dims=3),dims=3)
 data = cat(data,ydata,dims=3)
 
-fnc = joinpath(ads.path,"compiled-$(Dates.format(dtvec[1],dateformat"yyyymmdd"))_$(Dates.format(dtvec[end],dateformat"yyyymmdd")).nc")
-ds = NCDataset(fnc,"w",attrib=attribs[1])
+fnc = joinpath(ads.path,"diurnal-$(vname)-$(Dates.format(dtvec[1],dateformat"yyyymmdd"))_$(Dates.format(dtvec[end],dateformat"yyyymmdd")).nc")
+ds = NCDataset(fnc,"c",attrib=attribs[1])
 
-defDim(ds,"height",596)
-defDim(ds,"time",  21600)
+defDim(ds,"height",nz)
+defDim(ds,"time",  nt)
 defDim(ds,"month", 13)
 
 ncz  = defVar(ds,"height",Float32,("height",),attrib=attribs[2])
 nct  = defVar(ds,"time",Int32,("time",),attrib=attribs[3])
 ncwc = defVar(ds,vname,Float64,("height","time","month"),attrib=attribs[4])
+ncin = defVar(ds,"number",Int32,("height","time","month"))
 
 ncz[:] = z
 nct[:] = collect(0 : 4 : 86400)[1:(end-1)]
 ncwc[:,:,:] = data
+ncin[:,:,:] = cat(inum,sum(inum,dims=3),dims=3)
 
 close(ds)
